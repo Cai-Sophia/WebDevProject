dca697102e02d05ce7c28a4f2378906b4eccaacb
Sophia Cai <cai_sophia@outlook.com>
2024-05-02 09:13:59 -0400

    updating homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,25 +2,23 @@ import React, { useState, useEffect } from "react";
 import { categories, wordPositions } from '../assets/data';
 
 const HomePage = () => {
-    const [visibleWords, setVisibleWords] = useState({});
+  const [visibleWords, setVisibleWords] = useState({});
 
-    useEffect(() => {
-      const interval = setInterval(() => {
-        // Randomly select a word index to fade in or out
-        const randomIndex = Math.floor(Math.random() * categories.length);
-        setVisibleWords((prevVisibleWords) => {
-          const updatedWords = { ...prevVisibleWords };
-          // Toggle visibility for the selected word index
-          updatedWords[randomIndex] = !updatedWords[randomIndex];
-          return updatedWords;
-        });
-      }, 2000); // Set the interval duration (milliseconds)
-      return () => clearInterval(interval); // Cleanup interval on component unmount
-    }, [categories]);
-    
-    return (
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * categories.length);
+      setVisibleWords((prevVisibleWords) => {
+        const updatedWords = { ...prevVisibleWords };
+        updatedWords[randomIndex] = !updatedWords[randomIndex];
+        return updatedWords;
+      });
+    }, 1000); 
+    return () => clearInterval(interval); 
+  }, [categories]);
+  
+  return (
     <div className="home">
-      <div className="home-container">
+      <div className="word-container">
         {categories.map((category, index) => (
           <div
             key={index}
@@ -29,12 +27,14 @@ const HomePage = () => {
               left: wordPositions[index].left,
               top: wordPositions[index].top,
               fontSize: wordPositions[index].fontSize
-            }}
-          >
+            }}>
             {category}
           </div>
-        ))}
+          ))}
         <div className="title">DISCOVER NEW GAMES...</div>
+      </div>
+      <div className="top-games">
+        <div className="top-games-title">TOP GAMES</div>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,20 +5,19 @@ const NavBar = () => {
         <div id='nav-parent'>
             <div id='nav-bar'>
                 <div id='nav-buttons'>
-                    <i class="fa-solid fa-bars" style={{fontSize:'35px'}}></i>
-                    <i class="fa-solid fa-house" style={{fontSize:'32px'}}></i>
+                    <i class="fa-solid fa-bars" style={{fontSize:'25px'}}></i>
+                    <i class="fa-solid fa-house" style={{fontSize:'23px'}}></i>
                 </div>
                 <div id='search-query'>
                     <div id='dropdown-menu'>
                         <button className="dropbtn">SEARCH BY</button>
                         <div className="dropdown-content">
-                            <a>Name</a>
-                            <a>Category</a>
-                            <a>Platform</a> 
+                            <a>NAME</a>
+                            <a>CATEGORY</a>
+                            <a>PLATFORM</a> 
                         </div>
                     </div>
                     <input className="search-bar" type="text" placeholder="ENTER TEXT" />
-
                 </div>
                 <div>FAVORITES</div>
             </div>
