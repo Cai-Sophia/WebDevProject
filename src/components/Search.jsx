3b5d47b48cd9bf463b102f91abb517ca8da27ee8
Sophia Cai <cai_sophia@outlook.com>
2024-05-04 16:04:19 -0400

    pushing changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,35 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { getAccessToken } from '../utility/auth.js'; 
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Search = () => {
     const [searchResults, setSearchResults] = useState([]);
     const [searchTerm, setSearchTerm] = useState(''); 
-    const navigate = useNavigate();
+    const params = useParams();
 
     useEffect(() => {
         const searchRequest = async (searchTerm) => {
-            try {
-                const response = await fetch('http://localhost:8000/search', {
-                    method: 'POST',
-                    headers: {
-                      'Client-ID': 'gynkg0zhmuv2xlwdxxhq0fb8v6na9w',
-                      'Authorization': `Bearer ${getAccessToken()}`, 
-                      'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ searchTerm })
-                });
-                const data = await response.json();
-                setSearchResults(data.ids);
-            } catch (error) {
-                console.error('Error:', error);
-            }
+            const response = await fetch('http://localhost:8000/search', {
+                method: 'POST',
+                headers: {
+                    'Client-ID': 'gynkg0zhmuv2xlwdxxhq0fb8v6na9w',
+                    'Authorization': `Bearer ${getAccessToken()}`, 
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ searchTerm })
+            });
+            const data = await response.json();
+            console.log(data)
+            setSearchResults(data.ids);
         };
         searchRequest(searchTerm);
-    }, [searchTerm]);
+    }, [params.term]);
 
     const handleClick = (id) => {
-        navigate(`/games/${id}`);
+        // Handle click event
     };
 
     return (
