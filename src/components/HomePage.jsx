896973df2ee943fee23b1b40f60a2b20d2d219ca
Sophia Cai <cai_sophia@outlook.com>
2024-05-13 02:09:46 -0400

    changes

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,6 +9,8 @@ const HomePage = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
 
+  const [gameFavorites, setGameFavorites] = useState({});
+
   useEffect(() => {
     const interval = setInterval(() => {
       const randomIndex = Math.floor(Math.random() * categories.length);
@@ -57,34 +59,80 @@ const HomePage = () => {
     })
   }
 
+  useEffect(() => {
+    fetch('http://localhost:8000/test')
+      .then(response => response.json())
+      .then(data => setFavorites(data))
+      .catch(error => console.error('Error fetching favorite games:', error));
+  }, []);
+
   const handleFavoriteClick = async (game) => {
-    if (favorites.find(favorite => favorite.name === game.name)) {
-      setErrorMessage('Game already added to favorites');
-      return;
+    const response = await fetch('http://localhost:8000/test');
+    if (!response.ok) {
+      throw new Error('Failed to fetch favorite games');
     }
+    const newFavorites = await response.json();
+    const matchingFavorite = newFavorites.find(favorite => favorite.name === game.name);
+  
+    if (matchingFavorite) {
+      try {
+        handleRemoveFavorite(matchingFavorite._id);
+      } catch (error) {
+        console.error('Error fetching favorite games:', error);
+      }
+      setGameFavorites(prevGameFavorites => ({
+        ...prevGameFavorites,
+        [game.id]: false // Update favorited status of this game to false
+      }));
+    } 
+    else {
+      try {
+        const response = await fetch('http://localhost:8000/insert-favorite', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: game.name,
+            url: game.url,
+            coverUrl: game.cover?.url,
+          }),
+        });
+        if (response.ok) {
+          setFavorites(prevFavorites => [...prevFavorites, game]);
+          setSuccessMessage('Game added to favorites');
+          console.log('Favorite game added successfully');
+          setGameFavorites(prevGameFavorites => ({
+            ...prevGameFavorites,
+            [game.id]: true // Update favorited status of this game to false
+          }));
+        } else {
+          console.error('Failed to add favorite game');
+        }
+      } catch (error) {
+        console.error('Error adding favorite game:', error);
+      }
+    }
+  };  
+  
+  const handleRemoveFavorite = async (gameId) => {
     try {
-      const response = await fetch('http://localhost:8000/insert-favorite', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name: game.name,
-          url: game.url,
-          coverUrl: game.cover.url,
-        }),
+      const response = await fetch(`http://localhost:8000/remove-favorite/${gameId}`, {
+        method: 'DELETE',
       });
       if (response.ok) {
-        setFavorites([...favorites, game]);
-        setSuccessMessage('Game added to favorites');
-        console.log('Favorite game added successfully');
+        console.log('Favorite game removed successfully');
+        // Remove the game from the state
+        setFavorites(prevFavorites => prevFavorites.filter(favorite => favorite._id !== gameId));
       } else {
-        console.error('Failed to add favorite game');
+        console.error('Failed to remove favorite game');
       }
     } catch (error) {
-      console.error('Error adding favorite game:', error);
+      console.error('Error removing favorite game:', error);
     }
   };
+
+  
 
   return (
     <div className="home">
@@ -100,7 +148,7 @@ const HomePage = () => {
             }}>
             {category}
           </div>
-          ))}
+        ))}
         <div className="title">DISCOVER NEW GAMES...</div>
       </div>
       <div className="random-games">
@@ -114,15 +162,15 @@ const HomePage = () => {
                 </div>
                 <h3 className="game-title">{game.name}</h3>
               </a>
-              <button onClick={() => handleFavoriteClick(game)}>
-                <i className="fas fa-heart"></i> Favorite
+              <button className={`favorite-button ${gameFavorites[game.id] ? 'favorited' : ''}`} onClick={() => handleFavoriteClick(game)}>
+                <i className="fas fa-heart"></i>
               </button>
             </div>
           ))}
         </div>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        {successMessage && <p className="success-message">{successMessage}</p>}
       </div>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      {successMessage && <p className="success-message">{successMessage}</p>}
     </div>
   );
 };
